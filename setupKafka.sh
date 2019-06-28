f1bc19138aba2dc4729df46cae34f02767f326a2
andy <andrew.lees11@ibm.com>
2019-06-28 14:14:33 +0100

    use adoptopenjdk8 in travis

--- setupKafka.sh
+++ setupKafka.sh
@@ -4,21 +4,26 @@ set -o verbose
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew update
-    brew cask reinstall java
+    brew cask install homebrew/cask-versions/adoptopenjdk8
     brew install kafka
     brew install librdkafka
     brew services start zookeeper
     brew services start kafka
     sleep 9
 else
-    apt-get update
-    apt-get install -y wget
-    apt-get install librdkafka-dev
-    wget https://www-eu.apache.org/dist/kafka/2.2.0/kafka_2.12-2.2.0.tgz -O -O kafka.tgz
+    wget http://www.us.apache.org/dist/kafka/0.8.2.1/kafka_2.10-0.8.2.1.tgz -O kafka.tgz
     mkdir -p kafka && tar xzf kafka.tgz -C kafka --strip-components 1
-    cd kafka
-    ./bin/zookeeper-server-start.sh config/zookeeper.properties &
-    sleep 3
-    ./bin/kafka-server-start.sh config/server.properties &
-    - sleep 5
+    nohup bash -c "cd kafka && bin/zookeeper-server-start.sh config/zookeeper.properties &"
+    nohup bash -c "cd kafka && bin/kafka-server-start.sh config/server.properties &"
+    sleep 5
+    # apt-get update
+    # apt-get install -y wget
+    # apt-get install librdkafka-dev
+    # wget https://www-eu.apache.org/dist/kafka/2.2.0/kafka_2.12-2.2.0.tgz -O -O kafka.tgz
+    # mkdir -p kafka && tar xzf kafka.tgz -C kafka --strip-components 1
+    # cd kafka
+    # ./bin/zookeeper-server-start.sh config/zookeeper.properties &
+    # sleep 3
+    # ./bin/kafka-server-start.sh config/server.properties &
+    # - sleep 5
 fi
