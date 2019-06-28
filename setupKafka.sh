65abfab518b193a125e830195629ed6012f2dcf9
andy <andrew.lees11@ibm.com>
2019-06-28 14:56:54 +0100

    Add confluent to install

--- setupKafka.sh
+++ setupKafka.sh
@@ -13,9 +13,10 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 else
     apt-get update
     apt-get install -y wget
-    apt-get install -y librdkafka-dev
+    wget -qO - https://packages.confluent.io/deb/5.2/archive.key | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://packages.confluent.io/deb/5.2 stable main"
+    sudo apt-get update && sudo apt-get install confluent-community-2.12
     apt install librdkafka-dev
-
     wget https://www-eu.apache.org/dist/kafka/2.2.0/kafka_2.12-2.2.0.tgz -O kafka.tgz
     mkdir -p kafka && tar xzf kafka.tgz -C kafka --strip-components 1
     nohup bash -c "cd kafka && bin/zookeeper-server-start.sh config/zookeeper.properties &"
