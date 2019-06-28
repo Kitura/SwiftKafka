54a3a5d01513a38f448aa703920b014e2cd85a7a
andy <andrew.lees11@ibm.com>
2019-06-28 13:56:33 +0100

    Update kafka start instructions

--- setupKafka.sh
+++ setupKafka.sh
@@ -4,7 +4,7 @@ set -o verbose
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew update
-    brew cask install java
+    brew cask reinstall java
     brew install kafka
     brew install librdkafka
     brew services start zookeeper
@@ -12,10 +12,13 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     sleep 9
 else
     apt-get update
-    apt-get install -y wget librdkafka-dev
-    wget http://mirror.ox.ac.uk/sites/rsync.apache.org/kafka/2.2.0/kafka_2.12-2.2.0.tgz -O kafka.tgz
+    apt-get install -y wget
+    apt-get install librdkafka-dev
+    wget https://www-eu.apache.org/dist/kafka/2.2.0/kafka_2.12-2.2.0.tgz -O -O kafka.tgz
     mkdir -p kafka && tar xzf kafka.tgz -C kafka --strip-components 1
-    nohup bash -c "cd kafka && bin/zookeeper-server-start.sh config/zookeeper.properties &"
-    nohup bash -c "cd kafka && bin/kafka-server-start.sh config/server.properties &"
-    sleep 9
+    cd kafka
+    ./bin/zookeeper-server-start.sh config/zookeeper.properties &
+    sleep 3
+    ./bin/kafka-server-start.sh config/server.properties &
+    - sleep 5
 fi
