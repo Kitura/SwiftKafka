52c1c3fe960d63ab1bc330b43678fc3eb3844e71
David Jones <djones6@uk.ibm.com>
2019-07-04 10:32:59 +0100

    Adjust network name

--- setupKafka.sh
+++ setupKafka.sh
@@ -23,7 +23,7 @@ else
         cd examples/cp-all-in-one/
         docker-compose up -d --build
         docker-compose ps
-        # List active networks - confirm name of kafka bridge network (cpallinone_default)
+        # List active networks - confirm name of kafka bridge network (cp-all-in-one_default)
         docker network ls
     else
         #
