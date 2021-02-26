c1eb7abc54ec5dbba35da8ceac6dc58a7117c2f9
Kacper Kawecki <kacper@kawecki.pl>
2021-02-26 12:46:16 +0100

    Return Result for create topic API

--- Sources/SwiftKafka/KafkaAdmin.swift
+++ Sources/SwiftKafka/KafkaAdmin.swift
@@ -10,25 +10,44 @@ public class KafkaAdmin {
     let kafkaHandle: OpaquePointer
     
     private struct ErrorMapping: Error {
-        let error: KafkaError
+        let kafkaError: KafkaError
         let topicName: String
+    }
+
+    public struct KafkaTopicSpecificationError: Error {
+        let kafkaError: KafkaError
+        let topicSpecification: KafkaTopicSpecification
     }
 
     init(client: KafkaClient) {
         self.kafkaHandle = client.kafkaHandle
     }
 
-    //// Create topic if not exists
-    /// - Parameter topic: name of the topic to create
-    public func createTopics(topicNames: [String], numPartitions: Int32 = 1, replicationFactor: Int32 = -1, timeout: Int32 = 5000) throws -> [KafkaTopicSpecification]  {
+    /// Create topics with basic setup
+    ///
+    /// - Parameters:
+    ///   - topicNames: Array of topic names of which will be created
+    ///   - numPartitions: Numer of partitions per topic. Default is 1
+    ///   - replicationFactor: Topic replication factor. Set -1 to use cluster value. Default is -1
+    ///   - timeout: Time in ms for how long it should wait for replay from kafka before throwing error. Default is 5000 (5s)
+    /// - Returns: Array of result with KafkaTopicSpecification or KafkaTopicSpecificationError
+    /// - Throws: KafkaError
+    public func createTopics(topicNames: [String], numPartitions: Int32 = 1, replicationFactor: Int32 = -1, timeout: Int32 = 5000) throws -> [Result<KafkaTopicSpecification, KafkaTopicSpecificationError>]  {
         let topics = try topicNames.map { name -> KafkaTopicSpecification in
             try KafkaTopicSpecification(name: name, numPartitions: numPartitions, replicationFactor: replicationFactor)
         }
-        return try createTopics(topics: topics)
+        return try createTopics(topicSpecifications: topics)
     }
 
-    public func createTopics(topics: [KafkaTopicSpecification], timeout: Int32 = 5000) throws -> [KafkaTopicSpecification] {
-        var topicsPointers = topics.map { topic -> OpaquePointer? in topic.pointer }
+    /// Create topics
+    ///
+    /// - Parameters:
+    ///   - topicSpecifications: Array of topic specification which will be created
+    ///   - timeout: Time in ms for how long it should wait for replay from kafka before throwing error. Default is 5000 (5s)
+    /// - Returns: Array of result with KafkaTopicSpecification or KafkaTopicSpecificationError
+    /// - Throws: KafkaError
+    public func createTopics(topicSpecifications: [KafkaTopicSpecification], timeout: Int32 = 5000) throws -> [Result<KafkaTopicSpecification, KafkaTopicSpecificationError>] {
+        var topicsPointers = topicSpecifications.map { topic -> OpaquePointer? in topic.pointer }
 
         let queue = rd_kafka_queue_new(kafkaHandle)
         defer { rd_kafka_queue_destroy(queue) }
@@ -38,7 +57,7 @@ public class KafkaAdmin {
 
         rd_kafka_CreateTopics(kafkaHandle, &topicsPointers, topicsPointers.count, options, queue)
         guard let event = rd_kafka_queue_poll(queue, timeout) else {
-            throw KafkaError(description: "Haven't received replay form kafka")
+            throw KafkaError(description: "Haven't received replay form kafka on time!")
         }
         defer { rd_kafka_event_destroy(event) }
 
@@ -46,20 +65,22 @@ public class KafkaAdmin {
         guard errorCode.rawValue == 0 else {
             throw KafkaError.init(rawValue: Int(errorCode.rawValue))
         }
+
         let result = rd_kafka_event_CreateTopics_result(event)
         var count: Int = 0
         let cTopicResults = rd_kafka_CreateTopics_result_topics(result, &count)
         let topicResults = topicsToResult(topicResults: cTopicResults, count: count)
-        for res in topicResults {
-            switch res {
+        let topicMaps = topicSpecifications.reduce(into: [:]) { dictionary, topicSpecification in dictionary[topicSpecification.name] = topicSpecification }
+        
+        return topicResults.map { result -> Result<KafkaTopicSpecification, KafkaTopicSpecificationError> in
+            switch result {
             case .success(let name):
-                    print("Sucessffuly created topic \(name)")
-            case .failure(let errMapping):
-                print("Failed to create topic \(errMapping.topicName) with error \(errMapping.error)")
+                return .success(topicMaps[name]!)
+            case .failure(let error):
+                KafkaClient.logger.warning("Failed to create topic \(error.topicName) with error \(error.kafkaError)")
+                return .failure(KafkaTopicSpecificationError(kafkaError: error.kafkaError, topicSpecification: topicMaps[error.topicName]!))
             }
         }
-        
-        return topics
     }
     
     private func topicsToResult(topicResults: UnsafeMutablePointer<OpaquePointer?>?, count: Int) -> [Result<String, ErrorMapping>] {
@@ -72,7 +93,7 @@ public class KafkaAdmin {
             if errorCode.rawValue == 0 {
                 res = .success(name)
             } else {
-                res = .failure(ErrorMapping(error: KafkaError(rawValue: Int(errorCode.rawValue)), topicName: name))
+                res = .failure(ErrorMapping(kafkaError: KafkaError(rawValue: Int(errorCode.rawValue)), topicName: name))
             }
             result.append(res)
         }
