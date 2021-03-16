7ff912d8a2b32b701bd490c4ec2ae9a83c6cce9c
Kacper Kawecki <kacper@letsbuild.com>
2021-03-16 17:05:16 +0400

    Fix includes for C helper

--- Sources/Ckafkahelper/include/kafkahelper.h
+++ Sources/Ckafkahelper/include/kafkahelper.h
@@ -4,7 +4,13 @@
 
 #ifndef SWIFTKAFKA_KAFKAHELPER_H
 #define SWIFTKAFKA_KAFKAHELPER_H
-#include <librdkafka/rdkafka.h>
+
+#ifdef __APPLE__
+#include "/usr/local/include/librdkafka/rdkafka.h"
+#else
+#include "/usr/include/librdkafka/rdkafka.h"
+#endif //__APPLE__
+
 #include <stdlib.h>
 
 static const rd_kafka_topic_result_t * topic_result_by_idx (const rd_kafka_topic_result_t **topics, size_t cnt, size_t idx) {
